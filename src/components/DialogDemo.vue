0cbb89fdd43964af5caede0f4be44331ca365302
he <1921220510@qq.com>
2021-03-13 10:40:18 +0800

    dialog点击关闭功能完成

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,10 +1,31 @@
 <template>
-  dialog
+  <h2>Dialog示例</h2>
+  <h1>示例一</h1>
+  <q-button @click="toggle">toggle</q-button>
+  <q-dialog v-model:visible="x" :closeOnclickOverlay="false" :ok='f1' :cancel='f2'>
+  </q-dialog>
 </template>
 
 <script>
+import { ref } from 'vue'
+import QButton from '../lib/QButton.vue'
+import QDialog from '../lib/QDialog.vue'
 export default {
-  name:'DialogDemo'
+  components: { QDialog, QButton },
+  name:'DialogDemo',
+  setup(){
+    const x = ref(false)
+    const toggle = () =>{
+      x.value = !x.value
+    }
+    const f1 = function(){
+      return false
+    }
+    const f2 = () => {
+    }
+    return {x,toggle,f1,f2}
+  }
+
 }
 </script>
 
